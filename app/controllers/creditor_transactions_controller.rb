e44ad5dff50d8a8ee3f0a73944004909c5eb97d0
Kiichiro-T <tennis_0503@outlook.jp>
2019-12-17 14:44:14 +0900

    Transaction一覧を完成

--- app/controllers/creditor_transactions_controller.rb
+++ app/controllers/creditor_transactions_controller.rb
@@ -1,20 +1,25 @@
 class CreditorTransactionsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   
-    def new
-      @transaction = CreditorTransaction.new
-    end
-  
-    def create
-      @transaction = CreditorTransaction.new(transaction_params)
-      if @transaction.save
-        flash[:success] = "作成成功！"
-        redirect_to root_url
-      else
-        render 'new'
-      end
-    end
+  def index
+    @transactions = CreditorTransaction.where(debtor_id: current_user.id)
+  end
 
+  def new
+    @transaction = CreditorTransaction.new
+  end
+  
+  def create
+    @transaction = CreditorTransaction.new(transaction_params)
+    if @transaction.save
+      flash[:success] = "作成成功！"
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
+
+  private
     def transaction_params
       params.require(:creditor_transaction).permit(:debtor_id, :creditor_id, :debt, :repayment)
     end
--- app/controllers/debtor_transactions_controller.rb
+++ app/controllers/debtor_transactions_controller.rb
@@ -1,19 +1,25 @@
 class DebtorTransactionsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+
+  def index
+    @transactions = DebtorTransaction.where(creditor_id: current_user.id)
+  end
   
-    def new
-      @transaction = DebtorTransaction.new
-    end
+  def new
+    @transaction = DebtorTransaction.new
+  end
   
-    def create
-      @transaction = DebtorTransaction.new(transaction_params)
-      if @transaction.save
-        flash[:success] = "作成成功！"
-        redirect_to root_url
-      else
-        render 'new'
-      end
+  def create
+    @transaction = DebtorTransaction.new(transaction_params)
+    if @transaction.save
+      flash[:success] = "作成成功！"
+      redirect_to root_url
+    else
+      render 'new'
     end
+  end
+
+  private
 
     def transaction_params
       params.require(:debtor_transaction).permit(:debtor_id, :creditor_id, :debt, :repayment)
